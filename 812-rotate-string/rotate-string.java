4291f9384e3f407e01660c92ee812356cfb74dcd
Divya Dharshini .K <ddharshini952@gmail.com>
2026-09-24 12:53:29 +0530

    Time: 1 ms (98.32%) | Memory: 43.6 MB (16.12%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,12 +1,28 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()){
+        if (s.length() != goal.length()) {
             return false;
         }
-        String con=s+s;
-        if(con.contains(goal)){
-            return true;
+
+        String con = s + s;
+        int n = goal.length();
+
+        for (int i = 0; i < n; i++) {
+            if (con.charAt(i) == goal.charAt(0)) {
+                int start = i;
+                int index = 0;
+
+                while (index < n && con.charAt(start) == goal.charAt(index)) {
+                    index++;
+                    start++;
+                }
+
+                if (index == n) {
+                    return true;
+                }
+            }
         }
+
         return false;
-    }   
+    }
 }
